42a35c92ef4fc655a4468128eedba45796036f5c
slee713 <steven.lee1393@gmail.com>
2020-07-14 17:26:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,21 @@
 class Dog
-    def initialize(name, breed)
-        @name = name
-        @breed = breed
-    end
+    
 
-    def name=(name)
-        @name = name
+    def name=(new_name)
+        @name = new_name
     end
 
     def name 
         @name
     end
     
-    def breed=(breed)
-        @breed = breed
+    def breed=(new_breed)
+        @breed = new_breed
     end
 
     def breed
         @breed
     end
-    
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,20 @@
 class Person
-    def initialize(name, job)
-        @name = name
-        @job = job
-    end
-
-    def name=(name)
-        @name = name
+    
+    def name=(new_name)
+        @name = new_name
     end
 
     def name
         @name
     end
 
-    def job=(job)
-        @job = job
+    def job=(new_job)
+        @job = new_job
     end
 
     def job
         @job
     end
-    
+
     
 end
